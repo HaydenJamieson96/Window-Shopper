a3e6947cc88a9b970bf5812b0832dc8d6e379512
HaydenJamieson96 <32482976+HaydenJamieson96@users.noreply.github.com>
2017-12-20 12:16:32 +0000

    Initial commit

---
+++ Window Shopper/Controller/MainVC.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Window Shopper
+//
+//  Created by TheAppExperts on 12/20/17.
+//  Copyright © 2017 TheAppExperts. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    @IBOutlet weak var wageTxt: CurrencyTxtField!
+    @IBOutlet weak var priceTxt: CurrencyTxtField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let calcBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
+        calcBtn.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        calcBtn.setTitle("Calculate", for: .normal)
+        calcBtn.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        calcBtn.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside)
+        
+        wageTxt.inputAccessoryView = calcBtn
+        priceTxt.inputAccessoryView = calcBtn
+    }
+
+    @objc func calculate() {
+        
+    }
+
+}
+
